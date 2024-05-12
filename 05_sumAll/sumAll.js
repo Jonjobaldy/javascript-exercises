8470aea0c45005b26359bab1adec7eb9fb6d8f36
Jonjobaldy <129899335+Jonjobaldy@users.noreply.github.com>
2024-05-12 11:18:02 +0100

    Completed 04 - used Array instead of just for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (A, B) {
+    if (A > B) {
+        const C = A;
+        A = B;
+        B = C;
+    }
 
+    if (A < 0 || B < 0) {
+        return "ERROR"
+    }
+
+    if (!Number.isInteger(A) || !Number.isInteger(B)){
+        return "ERROR"
+    }
+
+    let array = [];
+    for (let i = A; i <= B; i++) {
+        array.push(i);
+    }
+    let sum = 0;
+    for (const item of array) {
+        sum += item;
+    }
+    return sum;
 };
 
 // Do not edit below this line
