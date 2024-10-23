69f9b2f1b766db8a4f0234116af8a29ce3d575f0
Jonjobaldy <129899335+Jonjobaldy@users.noreply.github.com>
2024-10-23 18:11:01 +0100

    COmpleted 09_PALINDROMES

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let lowerString = word.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, '');
+    
+    
+    let reverseString = lowerString
+                            .split("")
+                            .reverse()
+                            .join("");
 
+    return reverseString === lowerString;
+
+    console.log(lowerString);
+    console.log(reverseString);
 };
 
 // Do not edit below this line
