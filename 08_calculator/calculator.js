41613a2be3fd0562edbaed2143f7910b3218f21f
Jonjobaldy <129899335+Jonjobaldy@users.noreply.github.com>
2024-10-23 17:37:51 +0100

    completed 08_CALCULATOR

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(sumArray, 0);
+
+  function sumArray(accumulator, element){
+    return accumulator + element;
+  }
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce(multiplyArray);
 
+  function multiplyArray(accumulator, element){
+    return accumulator * element;
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0){
+    return 1;
+  } 
+  let answer = 1;
+  for (let i = a; i > 0; i--){
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
