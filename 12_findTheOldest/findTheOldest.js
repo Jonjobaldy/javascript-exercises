83821a5dc1c7a8631593658b427f0821d4e937c0
Jonjobaldy <129899335+Jonjobaldy@users.noreply.github.com>
2024-10-24 11:12:58 +0100

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const currentYear = new Date().getFullYear();
+
+    const oldestPerson = people.reduce((oldest, current) => {
+            const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+
+            const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        if (currentAge > oldestAge) {
+            return current;
+        } else {
+            return oldest;
+        }
+    });
+
+    return oldestPerson;
 
 };
 
