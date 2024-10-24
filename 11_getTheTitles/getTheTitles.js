ce95044396b9e3dd0255f6645509ba42098d9e54
Jonjobaldy <129899335+Jonjobaldy@users.noreply.github.com>
2024-10-24 09:36:05 +0100

    Completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titleNames = books.map(books => books.title);
+    return titleNames;
 };
 
 // Do not edit below this line
